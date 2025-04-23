a92e01a34aae929efea5a3f8f3a170923e1c432b
Pramodani Jayakodi <promzjaya42@gmail.com>
2025-04-23 13:22:26 +1000

    Updates to login, signup and splash screen UI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,18 @@
 import { Stack } from "expo-router";
+import { useFonts } from 'expo-font';
+import { robotoFonts } from '../styles/fonts';
+import { View, ActivityIndicator } from 'react-native';
 
 export default function RootLayout() {
-  return <Stack Stack screenOptions={{ headerShown: false }}/>;
+  const [fontsLoaded] = useFonts(robotoFonts);
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
---
+++ styles/fonts.tsx
@@ -0,0 +1,20 @@
+import {
+    Roboto_100Thin,
+    Roboto_300Light,
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+    Roboto_600SemiBold,
+    Roboto_900Black,
+  } from '@expo-google-fonts/roboto';
+  
+  export const robotoFonts = {
+    Roboto_100Thin,
+    Roboto_300Light,
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+    Roboto_600SemiBold,
+    Roboto_900Black,
+  };
+  
