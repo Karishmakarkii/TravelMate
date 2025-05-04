9d1876022b86636196a42813304e653e926e98d3
Adia0708 <adiamwasha@yahoo.com>
2025-05-04 12:51:10 +1000

    signup firebase auth

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,14 +1,60 @@
 import { View, Text, TextInput, TouchableOpacity, ImageBackground } from 'react-native';
 import { useRouter } from 'expo-router';
 import styles from '../styles/authStyles';
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from 'react';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getFirestore, addDoc, collection } from "firebase/firestore";
 
-import '../../firebase.js';
+import '../firebase.js';
 
-const {firebaseConfig} = require('../../firebase.js');
+const {firebaseConfig} = require('../firebase.js');
 
 export default function SignUpScreen() {
   const router = useRouter();
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
+  const [created, setCreate] = useState('');
+  const [pword, setPword] = useState('');
+  const [cpword, setCPword] = useState('');
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+
+  // Initialize db
+  const db = getFirestore(app);
+
+  async function writeToDB(email: String, name: String) {
+    try{
+      const docRef =  await addDoc(collection(db, "users"), {
+        email: email,
+        name: name
+      });
+    } catch(e) {
+      console.error("Error adding document: ", e)
+    }
+  }
+
+  function create() {
+    if (pword === cpword) {
+      createUserWithEmailAndPassword(auth, email, pword)
+      .then((userCredential) => {
+        // created
+        writeToDB(email, name);
+        setCreate("User successfully created!");
+        router.push('/login')
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setCreate("User not created!");
+      });
+    } else {
+      setCreate("Passwords do not match!");
+    }
+  }
 
   return (
     <ImageBackground
@@ -17,12 +63,13 @@ export default function SignUpScreen() {
     >
       <View style={styles.signupContainer}>
         <Text style={styles.loginTitle}>Sign Up</Text>
-        <TextInput placeholder="Name" style={styles.signupInput} />
-        <TextInput placeholder="Email" style={styles.signupInput} keyboardType="email-address" />
-        <TextInput placeholder="Password" style={styles.signupInput} secureTextEntry />
-        <TextInput placeholder="Confirm Password" style={styles.signupInput} secureTextEntry />
+        <Text>{created}</Text>
+        <TextInput placeholder="Name" style={styles.signupInput} onChangeText = {setName} value={name} />
+        <TextInput placeholder="Email" style={styles.signupInput} keyboardType="email-address" onChangeText = {setEmail} value={email} />
+        <TextInput placeholder="Password" style={styles.signupInput} secureTextEntry onChangeText = {setPword} value={pword} />
+        <TextInput placeholder="Confirm Password" style={styles.signupInput} secureTextEntry onChangeText = {setCPword} value={cpword} />
 
-        <TouchableOpacity style={styles.signupButton} onPress={() => router.back()}>
+        <TouchableOpacity style={styles.signupButton} onPress={() => create()}>
           <Text style={styles.signupButtonText}>Sign Up</Text>
         </TouchableOpacity>
 
