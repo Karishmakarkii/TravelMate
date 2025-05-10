2522e1666fc099fc8b28f629f3062459a38a53f0
Karishmakarkii <Karkikarishma143@gmail.com>
2025-05-10 17:00:21 +1000

    bug fixes for total time and travel mode and solve merge conflict

---
+++ components/mainLayout.tsx
@@ -0,0 +1,57 @@
+// components/MainLayout.tsx
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import Footer from './footer';
+import ProfileModal, { UserType } from './profileModal';
+import SettingsModal from './settingModal';
+import Header from './header';
+
+type MainLayoutProps = {
+  children: React.ReactNode;
+  showFooter?: boolean;
+  title?: string;
+};
+
+
+export default function MainLayout({ children, showFooter = true, title }: MainLayoutProps) {
+  const [isProfileVisible, setProfileVisible] = useState(false);
+  const [isSettingsVisible, setSettingsVisible] = useState(false);
+
+  const user: UserType = {
+    firstName: 'Pramodani',
+    lastName: 'Jayakodi',
+    phone: '+4534555555',
+    email: 'promzfedd@gmail.com',
+  };
+
+  return (
+    <View style={styles.container}>
+      <Header title={title} showMenu onOpenSettings={() => setSettingsVisible(true)}/>
+
+      <View style={styles.content}>
+        {children}
+      </View>
+
+      {showFooter && (
+        <Footer showBack onOpenProfile={() => setProfileVisible(true)} />
+      )}
+
+      <ProfileModal
+        isVisible={isProfileVisible}
+        onClose={() => setProfileVisible(false)}
+        user={user}
+      />
+
+      <SettingsModal
+        isVisible={isSettingsVisible}
+        onClose={() => setSettingsVisible(false)}
+        onOpenProfile={() => setProfileVisible(true)}
+      />
+    </View >
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  content: { flex: 1 },
+});
