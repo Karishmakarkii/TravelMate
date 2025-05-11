a2d3a7cb8ce630e72d950cc7024c3820e37b9109
Karishmakarkii <Karkikarishma143@gmail.com>
2025-05-11 22:03:02 +1000

    add: fix for header layout

--- app/home.tsx
+++ app/home.tsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, ImageBackground, ScrollView, Platform } from 'react-native';
 import { useRouter } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import styles from '../styles/authStyles';
 import Header from '../components/header';
 import Footer from '@/components/footer';
 import DropDownPicker from 'react-native-dropdown-picker';
 import ProfileModal, { UserType } from '../components/profileModal';
-
+import MainLayout from '../components/mainLayout';
 export default function HomeScreen() {
   const router = useRouter();
 
@@ -44,53 +45,56 @@ export default function HomeScreen() {
 
   return (
     <ImageBackground source={require('../assets/images/PagesImage.jpeg')} style={styles.background}>
-      <View style={{ flex: 1 }}>
-        <Header title="Home" onOpenSettings={() => {}} />
-        <ScrollView contentContainerStyle={[styles.homeContainer, { paddingBottom: 30 }]}>
-          <Text style={styles.homeTitle}>Set search radius</Text>
-          <DropDownPicker
-            open={radiusOpen}
-            value={radiusValue}
-            items={radiusItems}
-            setOpen={setRadiusOpen}
-            setValue={setRadiusValue}
-            setItems={setRadiusItems}
-            placeholder="Select"
-            style={styles.dropdown}
-            dropDownContainerStyle={styles.dropdownContainer}
-            zIndex={3000}
-            zIndexInverse={1000}
-          />
+      <SafeAreaView style={{ flex: 1 }}>
+        <MainLayout title="Home">
+          <View style={{ flex: 1 }}>
+            <ScrollView contentContainerStyle={[styles.homeContainer, { paddingBottom: 30 }]}>
+              <Text style={styles.homeTitle}>Set search radius</Text>
+              <DropDownPicker
+                open={radiusOpen}
+                value={radiusValue}
+                items={radiusItems}
+                setOpen={setRadiusOpen}
+                setValue={setRadiusValue}
+                setItems={setRadiusItems}
+                placeholder="Select"
+                style={styles.dropdown}
+                dropDownContainerStyle={styles.dropdownContainer}
+                zIndex={3000}
+                zIndexInverse={1000}
+              />
 
-          <Text style={styles.homeTitle}>Select transport mode</Text>
-          <DropDownPicker
-            open={transportOpen}
-            value={transportValue}
-            items={transportItems}
-            setOpen={setTransportOpen}
-            setValue={setTransportValue}
-            setItems={setTransportItems}
-            placeholder="Select"
-            style={styles.dropdown}
-            dropDownContainerStyle={styles.dropdownContainer}
-            zIndex={2000}
-            zIndexInverse={2000}
-          />
+              <Text style={styles.homeTitle}>Select transport mode</Text>
+              <DropDownPicker
+                open={transportOpen}
+                value={transportValue}
+                items={transportItems}
+                setOpen={setTransportOpen}
+                setValue={setTransportValue}
+                setItems={setTransportItems}
+                placeholder="Select"
+                style={styles.dropdown}
+                dropDownContainerStyle={styles.dropdownContainer}
+                zIndex={2000}
+                zIndexInverse={2000}
+              />
 
-          <TouchableOpacity 
-            style={styles.homeButton} 
-            onPress={() => router.push({
-              pathname: '/attractionList',
-              params: {
-                radius: radiusValue,
-                transportMode: transportValue
-              }
-            })}
-          >
-            <Text style={styles.homeButtonText}>Find Places</Text>
-          </TouchableOpacity>
-        </ScrollView>
-      </View>
+              <TouchableOpacity 
+                style={styles.homeButton} 
+                onPress={() => router.push({
+                  pathname: '/attractionList',
+                  params: {
+                    radius: radiusValue,
+                    transportMode: transportValue
+                  }
+                })}
+              >
+                <Text style={styles.homeButtonText}>Find Places</Text>
+              </TouchableOpacity>
+            </ScrollView>
+          </View>
+        </MainLayout>
+      </SafeAreaView>
     </ImageBackground>
   );
 }
